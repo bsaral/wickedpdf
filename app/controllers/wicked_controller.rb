bc32785dd72565946b7c727fda9cfbe92beb2aee
bsaral <betul.saral@bil.omu.edu.tr>
2013-12-05 20:12:24 +0200

    start

---
+++ app/controllers/wicked_controller.rb
@@ -0,0 +1,12 @@
+class WickedController < ApplicationController
+  def index
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render :pdf => "deneme", # pdf will download as my_pdf.pdf
+        :layout => 'pdf', # uses views/layouts/pdf.haml
+        :show_as_html => params[:debug].present? # renders html version if you set debug=true in URL
+      end
+    end
+  end
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wickedpdf::Application.config.session_store :cookie_store, key: '_wickedpdf_session'
